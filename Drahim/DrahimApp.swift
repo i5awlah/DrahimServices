37583cc89ac272687d1958539be366621cbd8517
Khawlah <khawlah_alrashed@hotmail.com>
2025-01-18 22:31:03 +0300

    Initial Commit

---
+++ Drahim/DrahimApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrahimApp.swift
+//  Drahim
+//
+//  Created by Khawlah on 18/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct DrahimApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
